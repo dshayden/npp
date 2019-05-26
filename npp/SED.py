33e2247a495cfb716b366be14bad122736c7bc28
David Hayden <dshayden@mit.edu>
2019-05-26 17:48:55 -0400

    Part, body translation, rotation

--- npp/SED.py
+++ npp/SED.py
@@ -1,4 +1,4 @@
-from . import util
+from . import util, sample
 
 import numpy as np, argparse, lie, warnings
 from scipy.linalg import block_diag
@@ -6,7 +6,7 @@ from sklearn.mixture import BayesianGaussianMixture as dpgmm
 from scipy.stats import multivariate_normal as mvn, invwishart as iw
 from scipy.special import logsumexp
 import du, du.stats # move useful functions from these to utils (catrnd)
-
+import functools
 
 import IPython as ip
 
@@ -39,7 +39,7 @@ def opts(**kwargs):
     o.dy = 3
   else:
     assert False, 'Only support se2 or se3'
-  o.dxGm = (o.dy+1, o.dy+1)
+  o.dxGm = [o.dy+1, o.dy+1]
 
   return o
 
@@ -84,7 +84,6 @@ def initPriorsDataDependent(o, y, **kwargs):
   # S prior
   rotS = kwargs.get('rotS', 1.5)
   dfS = kwargs.get('dfS', 10.0)
-  # expRotS_radian = rotS * (np.pi / 180.)
   expRotS_radian = d2r(rotS)
   o.H_S = ('iw', dfS, np.diag( np.concatenate((
     y_muAbsDiff * (dfS - o.dxA - 1),
@@ -128,7 +127,7 @@ def initXDataMeans(o, y):
   for t in range(T): x[t,:-1,-1] = y_mu[t]
   return x
 
-def initPartsAssoc(o, y, x, alpha, **kwargs):
+def initPartsAndAssoc(o, y, x, alpha, **kwargs):
   """ Initialize parts (theta, E, S) and associations z using static DP.
   
   INPUT
@@ -208,6 +207,352 @@ def initPartsAssoc(o, y, x, alpha, **kwargs):
   #     associate
   return z0, pi, theta0, E0
 
+def sampleTranslationX(o, y_t, z_t, x_t, theta_t, E, Q, x_tminus1, **kwargs):
+  """ Sample translation component d_x_t of x_t
+
+  INPUT
+    o (argparse.Namespace): Algorithm options
+    y_t (ndarray, [N_t, dy]): Observations at time t
+    z_t (ndarray, [N_t,]): Associations at time t
+    x_t (ndarray, dxGm): Current global latent dynamic, time t
+    theta_t (ndarray, [K, dxGm]): Part Dynamics, time t
+    E (ndarray, [K, dy, dy]): Part Local Observation Noise Covs
+    Q (ndarray, [dxA, dxA]): Global Dynamic Noise Cov
+    x_tminus1 (ndarray, dxGm): Previous global latent dynamic, time t
+
+  KEYWORD INPUTS
+    x_tplus1 (ndarray, dxGm): Global latent dynamic, time t+1
+
+  OUTPUT
+    d_x_t (ndarray, [dy,]): Sampled global k translation dynamic, time t
+  """
+  m = getattr(lie, o.lie)
+  K = theta_t.shape[0]
+
+  # Are we sampling from forward filter (past only) or full conditional?
+  x_tplus1 = kwargs.get('x_tplus1', None)
+  if x_tplus1 is None: noFuture = True
+  else: noFuture = False
+
+  # Only care about translation component of noise
+  Q_d = Q[:o.dy, :o.dy]
+
+  # Separate R, d
+  R_x_t, _ = m.Rt(x_t)
+  R_x_tminus1, d_x_tminus1 = m.Rt(x_tminus1)
+
+  # Sample d_x_t | R_x_t, MB(x_t) from linear Gaussian system
+  V_x_tminus1_x_t_inv = m.getVi( m.inv(x_tminus1).dot(x_t) )
+  V_x_tminus1_x_t = np.linalg.inv(V_x_tminus1_x_t_inv)
+  RV = R_x_tminus1.dot(V_x_tminus1_x_t)
+
+  ## incorporate N( d_{x_t} | ... )
+  mu = d_x_tminus1
+  Sigma = RV.dot(Q_d).dot(RV.T)
+
+  if not noFuture:
+    R_x_tplus1, d_x_tplus1 = m.Rt(x_tplus1)
+    V_x_t_x_tplus1_inv = m.getVi( m.inv(x_t).dot(x_tplus1) )
+    V_x_t_x_tplus1 = np.linalg.inv(V_x_t_x_tplus1_inv)
+    RV = R_x_t.dot(V_x_t_x_tplus1)
+    obsCov = RV.dot(Q_d).dot(RV.T)
+    mu, Sigma = inferNormalNormal(d_x_tplus1, obsCov, mu, Sigma)
+
+  ## incorporate \prod_n N( y_{tn} | ... )
+  ### pre-compute observation bias and covariance for each of K parts
+  obsCov = np.zeros((K, o.dy, o.dy))
+  bias = np.zeros((K, o.dy))
+  for k in range(K):
+    # if not isObserved[k]: continue
+    R_theta_tk, d_theta_tk = m.Rt(theta_t[k])
+    R2 = R_x_t.dot(R_theta_tk)
+    obsCov[k] = R2.dot(E[k]).dot(R2.T)
+    bias[k] = R_x_t.dot(d_theta_tk)
+
+  ### update with each observation
+  for n in range(y_t.shape[0]):
+    k = z_t[n]
+    if k >= 0:
+      mu, Sigma = inferNormalNormal(y_t[n], obsCov[k], mu, Sigma, b=bias[k])
+
+  ## sample d_x_t
+  d_x_t = mvn.rvs(mu, Sigma)
+
+  # return mu, Sigma
+  return d_x_t
+
+## Only valid for 0 correlation between rotation, translation
+def sampleTranslationTheta(o, y_tk, theta_tk, x_t, S_tminus1_k, E_k, theta_tminus1_k, **kwargs):
+  """ Sample translation component d_theta_tk of theta_tk
+
+  INPUT
+    o (argparse.Namespace): Algorithm options
+    y_tk (ndarray, [N_tk, dy]): Observations associated to part k at time t
+    theta_tk (ndarray, dxGm): Part k Local Dynamic (current estimate)
+    x_t (ndarray, dxGm): Current global latent dynamic, time t
+    S_tminus1_k (ndarray, [dxA, dxA]): Part k Local Dynamic Noise Cov 
+    E_k (ndarray, [dy, dy]): Part k Extent
+    theta_tminus1_k (ndarray, dxGm): Global latent dynamic, time t-1
+
+  KEYWORD INPUTS
+    theta_tplus1_k (ndarray, dxGm): Global latent dynamic, time t+1
+    S_tplus1_k (ndarray, [dxA, dxA]): Part k Local Dynamic Noise Cov, time t+1
+
+  OUTPUT
+    d_theta_tk (ndarray, [dy,]): Sampled part k translation dynamic, time t
+  """
+  m = getattr(lie, o.lie)
+
+  # Are we sampling from forward filter (past only) or full conditional?
+  theta_tplus1_k = kwargs.get('theta_tplus1_k', None)
+  if theta_tplus1_k is None: noFuture = True
+  else: noFuture = False
+
+  # Only different if we use noise approximations
+  S_tplus1_k = kwargs.get('S_tplus1_k', S_tminus1_k)
+
+  # Only care about translation component of noise
+  S_tminus1_kd = S_tminus1_k[:o.dy, :o.dy]
+  S_tplus1_kd = S_tplus1_k[:o.dy, :o.dy]
+
+  # Separate R, d
+  R_x_t, d_x_t = m.Rt(x_t)
+  R_theta_tk, d_theta_tk = m.Rt(theta_tk)
+  R_theta_tminus1_k, d_theta_tminus1_k = m.Rt(theta_tminus1_k)
+
+  # Sample d_theta_tk | R_theta_tk, ... from linear Gaussian system
+  V_theta_tminus1_k_theta_tk_inv = m.getVi(m.inv(theta_tminus1_k).dot(theta_tk))
+  RV = du.mrdivide(R_theta_tminus1_k, V_theta_tminus1_k_theta_tk_inv)
+
+  ## Incorporate N( d_{theta_tk} | ... )
+  mu = d_theta_tminus1_k
+  Sigma = RV.dot(S_tminus1_kd).dot(RV.T)
+
+  if not noFuture:
+    R_theta_tplus1_k, d_theta_tplus1_k = m.Rt(theta_tplus1_k)
+    V_theta_tk_tplus1_k_inv = m.getVi( m.inv(theta_tk).dot(theta_tplus1_k) )
+    RV = du.mrdivide(R_theta_tk, V_theta_tk_tplus1_k_inv)
+    obsCov = RV.dot(S_tplus1_kd).dot(RV.T)
+    mu, Sigma = inferNormalNormal(d_theta_tplus1_k, obsCov, mu, Sigma)
+
+  ## incorporate \prod_n N( y_{tn} | z_{tn}==k, ... )
+  H = R_x_t
+  bias = d_x_t
+  R2 = R_x_t.dot(R_theta_tk)
+  obsCov = R2.dot(E_k).dot(R2.T)
+
+  ## update with each observation
+  # TODO: can we do this without a loop?
+  for n in range(y_tk.shape[0]):
+    mu, Sigma = inferNormalNormal(y_tk[n], obsCov, mu, Sigma, b=bias, H=H)
+
+  # sample d_theta_tk
+  return mvn.rvs(mu, Sigma)
+
+def sampleRotationX(o, y_t, z_t, x_t, theta_t, E, Q, x_tminus1, **kwargs):
+  """ Sample translation component d_x_t of x_t
+
+  INPUT
+    o (argparse.Namespace): Algorithm options
+    y_t (ndarray, [N_t, dy]): Observations at time t
+    z_t (ndarray, [N_t,]): Associations at time t
+    x_t (ndarray, dxGm): Current global latent dynamic, time t
+    theta_t (ndarray, [K, dxGm]): Part Dynamics, time t
+    E (ndarray, [K, dy, dy]): Part Local Observation Noise Covs
+    Q (ndarray, [dxA, dxA]): Global Dynamic Noise Cov
+    x_tminus1 (ndarray, dxGm): Previous global latent dynamic, time t
+
+  KEYWORD INPUTS
+    x_tplus1 (ndarray, dxGm): Global latent dynamic, time t+1
+    nRotationSamples (int): length of markov chain for rotation slice sampler
+    w (float): characteristic width of slice sampler, def: np.pi / 100
+    P (float): max doubling iterations of slice sampler def: 10
+
+  OUTPUT
+    R_x_t (ndarray, [dy,]): Sampled global k translation dynamic, time t
+  """
+  m = getattr(lie, o.lie)
+  K = theta_t.shape[0]
+
+  nRotationSamples = kwargs.get('nRotationSamples', 10)
+  if nRotationSamples == 0:
+    R_x_t, _ = m.Rt(x_t)
+    return R_x_t 
+
+  # Get future values, if needed.
+  x_tplus1 = kwargs.get('x_tplus1', None)
+  if x_tplus1 is None: noFuture = True
+  else: noFuture = False
+
+  # Sample R_x_t | MB(R_x_t) using slice sampler
+  if o.lie == 'se2': rotIdx = np.arange(2,3)
+  elif o.lie == 'se3': rotIdx = np.arange(3,6) 
+
+  ## setup
+  zeroAlgebra = np.zeros(o.dxA)
+  zeroObs = np.zeros(o.dy)
+
+  # slice sampler parameters
+  w = kwargs.get('w', np.pi / 100) # characteristic width
+  P = kwargs.get('P', 10) # max doubling iterations
+
+  x_t = x_t.copy()
+  for idx in rotIdx:
+    p0 = m.algi(m.logm(m.inv(x_tminus1).dot(x_t)))
+
+    # inlined functional, making use of lots of closures
+    def slice_log_x_t_full_conditional(idx, p0, a):
+      p = p0.copy()
+      p[idx] = a
+
+      # full x_t from proposed angle a for given idx
+      x_t_proposal = x_tminus1.dot(m.expm(m.alg(p)))
+
+      ll = 0
+
+      # prior
+      ll += mvn.logpdf(
+        m.algi(m.logm(m.inv(x_tminus1).dot(x_t_proposal))),
+        zeroAlgebra,
+        Q,
+        allow_singular=True
+      )
+
+      # future observations, if available
+      if not noFuture:
+        ll += mvn.logpdf(
+          m.algi(m.logm(m.inv(x_t_proposal).dot(x_tplus1))),
+          zeroAlgebra,
+          Q,
+          allow_singular=True
+        )
+
+      # current y_t observations
+      for k in range(K):
+        z_tk = z_t == k
+        Nk = np.sum(z_tk)
+        if Nk == 0: continue
+
+        y_tk_world = y_t[z_tk]
+        T_part_world = m.inv( x_t_proposal.dot(theta_t[k]) )
+        y_tk_part = TransformPointsNonHomog(T_part_world, y_tk_world)
+        ll += np.sum(mvn.logpdf(y_tk_part, zeroObs, E[k], allow_singular=True))
+      return ll
+
+    # end full conditional functional
+    llFunc = functools.partial(slice_log_x_t_full_conditional, idx, p0)
+    x_t_sample_angles, _ll = sample.slice_univariate(llFunc, p0[idx],
+      w=w, P=P, nSamples=nRotationSamples, xmin=-np.pi, xmax=np.pi)
+
+    # Note: x_t_sample_angles are just univariate floats so we need to plug
+    #       new sampled angle into matrix repr
+    p = p0.copy()
+    p[idx] = x_t_sample_angles[-1]
+    x_t = x_tminus1.dot(m.expm(m.alg(p)))
+  # end loop over rotation coordinates
+
+  R_x_t, _ = m.Rt(x_t)
+  return R_x_t 
+
+def sampleRotationTheta(o, y_tk, theta_tk, x_t, S_tminus1_k, E_k, theta_tminus1_k, **kwargs):
+  """ sample R_theta_tk | MB(R_theta_tk) using slice sampler
+
+  INPUT
+    o (argparse.Namespace): Algorithm options
+    y_tk (ndarray, [N_tk, dy]): Observations associated to part k at time t
+    theta_tk (ndarray, dxGm): Part k Local Dynamic (current estimate)
+    x_t (ndarray, dxGm): Current global latent dynamic, time t
+    S_tminus1_k (ndarray, [dxA, dxA]): Part k Local Dynamic Noise Cov 
+    E_k (ndarray, [dy, dy]): Part k Extent
+    theta_tminus1_k (ndarray, dxGm): Global latent dynamic, time t-1
+
+  KEYWORD INPUTS
+    theta_tplus1_k (ndarray, dxGm): Global latent dynamic, time t+1
+    S_tplus1_k (ndarray, [dxA, dxA]): Part k Local Dynamic Noise Cov, time t+1
+    nRotationSamples (int): length of markov chain for rotation slice sampler
+    w (float): characteristic width of slice sampler, def: np.pi / 100
+    P (float): max doubling iterations of slice sampler def: 10
+
+  OUTPUT
+    R_theta_tk (ndarray, [dy, dy]): Sampled rotation estimate.
+  """
+  m = getattr(lie, o.lie)
+  nRotationSamples = kwargs.get('nRotationSamples', 10)
+  if nRotationSamples == 0:
+    R_theta_tk, _ = m.Rt(theta_tk)
+    return R_theta_tk
+
+  # Get future values, if needed.
+  theta_tplus1_k = kwargs.get('theta_tplus1_k', None)
+  if theta_tplus1_k is None: noFuture = True
+  else: noFuture = False
+  S_tplus1_k = kwargs.get('S_tplus1_k', S_tminus1_k)
+
+  if o.lie == 'se2': rotIdx = np.arange(2,3)
+  elif o.lie == 'se3': rotIdx = np.arange(3,6) 
+
+  ## setup
+  zeroAlgebra = np.zeros(o.dxA)
+  zeroObs = np.zeros(o.dy)
+
+  # slice sampler parameters
+  w = kwargs.get('w', np.pi / 100) # characteristic width
+  P = kwargs.get('P', 10) # max doubling iterations
+
+  theta_tk = theta_tk.copy() # don't modify passed parameter
+  for idx in rotIdx:
+    p0 = m.algi(m.logm(m.inv(theta_tminus1_k).dot(theta_tk)))
+
+    # inlined functional, making use of lots of closures
+    def slice_log_theta_tk_full_conditional(idx, p0, a):
+      p = p0.copy()
+      p[idx] = a
+
+      # full theta_tk from proposed angle a for given idx
+      theta_tk_proposal = theta_tminus1_k.dot(m.expm(m.alg(p)))
+
+      ll = 0
+
+      # prior
+      ll += mvn.logpdf(
+        m.algi(m.logm(m.inv(theta_tminus1_k).dot(theta_tk_proposal))),
+        zeroAlgebra,
+        S_tminus1_k,
+        allow_singular=True
+      )
+
+      # future observations, if available
+      if not noFuture:
+        ll += mvn.logpdf(
+          m.algi(m.logm(m.inv(theta_tk_proposal).dot(theta_tplus1_k))),
+          zeroAlgebra,
+          S_tplus1_k,
+          allow_singular=True
+        )
+
+      if y_tk.shape[0] > 0:
+        y_tk_world = y_tk
+        T_part_world = m.inv(x_t.dot(theta_tk_proposal))
+        y_tk_part = TransformPointsNonHomog(T_part_world, y_tk_world)
+
+        ll += np.sum(mvn.logpdf(y_tk_part, zeroObs, E_k, allow_singular=True))
+      return ll
+    # end full conditional functional
+
+    llFunc = functools.partial(slice_log_theta_tk_full_conditional, idx, p0)
+    theta_tk_sample_angles, _ll = sample.slice_univariate(llFunc, p0[idx],
+      w=w, P=P, nSamples=nRotationSamples, xmin=-np.pi, xmax=np.pi)
+
+    # Note: theta_tk_sample_angles are just univariate floats so we need to plug
+    #       new sampled angle into matrix repr
+    p = p0.copy()
+    p[idx] = theta_tk_sample_angles[-1]
+    theta_tk = theta_tminus1_k.dot(m.expm(m.alg(p)))
+  # end loop over rotation coordinates
+
+  R_theta_tk, _ = m.Rt(theta_tk)
+  return R_theta_tk
+
 def inferZ(o, y_t, pi, theta_t, E, x_t, mL_t, **kwargs):
   """ Sample z_{tn} | y_{tn}, pi, theta_t, E
 
@@ -248,7 +593,24 @@ def inferZ(o, y_t, pi, theta_t, E, x_t, mL_t, **kwargs):
   z[z==K] = -1
   return z
 
+def getComponentCounts(o, z, pi):
+  """ Count number of observations associated to each component.
 
+  INPUT
+    o (argparse.Namespace): Algorithm options
+    z (ndarray, [N_1,]): Associations
+    pi (ndarray, [K+1,]): Local association priors for each time
+
+  OUTPUT
+    Nk (ndarray, [K+1,]): Observation counts
+  """
+  K1 = len(pi)
+  Nk = np.zeros(K1, dtype=np.int)
+  uniq, cnts = np.unique(z[t], return_counts=True)
+  for u, c in zip(uniq, cnts):
+    uni = u if u != -1 else -1
+    Nk[uni] = c
+  return Nk
 
 # todo: move to utils
 def MakeRd(R, d):
@@ -280,3 +642,34 @@ def TransformPointsNonHomog(T, y):
   if y.ndim == 1: y = y[np.newaxis]
   yp = np.squeeze(y.dot(R.T) + d)
   return yp
+
+def inferNormalNormal(y, SigmaY, muX, SigmaX, H=None, b=None):
+  """ Conduct a conjugate Normal-Normal update on the below system:
+
+  p(x | y) \propto p(x)               p(y | x)
+                 = N(x | muX, SigmaX) N(y | Hx + b, SigmaY)
+
+  INPUT
+    y (ndarray, [dy,]): Observation
+    SigmaY (ndarray, [dy, dy]): Observation covariance
+    muX (ndarray, [dx,]): Prior mean
+    SigmaX (ndarray, [dx,dx]): Prior covariance
+    H (ndarray, [dy,dx]): Multiplicative dynamics
+    b (ndarray, [dy,]): Additive dynamics
+
+  OUPUT
+    mu (ndarray, [dx,]): Posterior mean
+    Sigma (ndarray, [dx,dx]): Posterior covariance
+  """
+  (dy, dx) = (y.shape[0], muX.shape[0])
+  if H is None: H = np.eye(dy,dx)
+  if b is None: b = np.zeros(dy)
+
+  SigmaXi = np.linalg.inv(SigmaX)
+  SigmaYi = np.linalg.inv(SigmaY)
+
+  SigmaI = SigmaXi + H.T.dot(SigmaYi).dot(H)
+  Sigma = np.linalg.inv(SigmaI)
+  mu = Sigma.dot(H.T.dot(SigmaYi).dot(y-b) + SigmaXi.dot(muX))
+
+  return mu, Sigma
---
+++ npp/sample.py
@@ -0,0 +1,113 @@
+import numpy as np
+from scipy.stats import expon
+import IPython as ip
+
+def slice_univariate(f, x0, **kwargs):
+  """ Univariate slice sampler.
+
+  INPUT
+    f (fcn): function f(x), evaluates log pdf
+    x0 (float): initial point
+
+  KEYWORD INPUT
+    nSamples (int): number of samples, default 10
+    w (float): doubling width, default 1
+    P (int): max doubling iterations, default 10
+    xmin (float): minimum for x support, default np.finfo(np.float).min
+    xmax (float): maximum for x support, default np.finfo(np.float).max
+    debug (bool): print debugging information, default False
+  
+  OUTPUT
+    xs (ndarray, [nSamples,]): samples
+    ll (ndarray, [nSamples,]): log-likelihood
+  """
+  w = kwargs.get('w', 1.0)
+  nSamples = kwargs.get('nSamples', 10)
+  P = kwargs.get('P', 10)
+  xmin = kwargs.get('xmin', np.finfo(np.float).min)
+  xmax = kwargs.get('xmax', np.finfo(np.float).max)
+  debug = kwargs.get('debug', False)
+  # debug = kwargs.get('debug', True)
+
+  def doubling(x, z):
+    # L = x - w*np.random.rand()
+    # R = L + w
+    L = np.maximum(x - w*np.random.rand(), xmin)
+    R = np.minimum(L+w, xmax)
+    K = P
+    while K>0 and (z<f(L) or z<f(R)):
+      if np.random.rand() < 0.5: L = np.maximum(L - (R-L), xmin)
+      else: R = np.minimum(R + (R-L), xmax)
+      if debug: print(f'In doubling loop, L: {L:.4f}, R: {R:.4f}, K: {K}')
+      K = K - 1
+    return L, R
+
+  def accept(x0, x1, z, L, R):
+    if debug: print(f'In accept: L: {L:.4f}, R: {R:.4f}, R-L: {(R-L):.4f}, 1.1*w: {(1.1*w):.4f}')
+    Lhat, Rhat = (L, R)
+    D = False
+    while (Rhat - Lhat) > (1.1 * w):
+      M = (Lhat + Rhat) / 2.
+      if (x0 < M and x1 >= M) or (x0 >= M and x1 < M): D = True
+      if x1 < M: Rhat = M
+      else: Lhat = M
+
+      if debug: print(f'In accept loop: Lhat: {Lhat:.4f}, Rhat: {Rhat:.4f}, M: {M:.4f}, Rhat-Lhat: {(Rhat-Lhat):.4f}, D: {D}')
+      if D and z >= f(Lhat) and z >= f(Rhat): return False
+    return True
+
+  def shrinkage(x0, z, L, R):
+    Lbar, Rbar = (L, R)   
+    while True:
+      if debug: print(f'In shrinkage loop, Lbar: {Lbar:.4f}, Rbar: {Rbar:.4f}')
+      x1 = Lbar + np.random.rand() * (Rbar - Lbar)
+      ll = f(x1)
+      if z < ll and accept(x0, x1, z, Lbar, Rbar): break
+      if x1 < x0: Lbar = x1
+      else: Rbar = x1
+    return x1, ll
+  
+  xs = np.zeros(nSamples)
+  ll = np.zeros(nSamples)
+  prevX = x0
+  for nS in range(nSamples):
+    z = f(prevX) - expon.rvs()
+    L, R = doubling(prevX, z)
+    xs[nS], ll[nS] = shrinkage(prevX, z, L, R)
+    prevX = xs[nS]
+
+  return xs, ll
+
+if __name__ == "__main__":
+  import matplotlib.pyplot as plt
+  from scipy.stats import multivariate_normal as mvn
+  import IPython as ip
+  from functools import partial
+  import du, du.stats
+  D = 2
+  Sigma = np.array([[10, 2], [2, 3]])
+  mu = np.zeros(2)
+  print('True Sigma\n', Sigma)
+  print('True mu\n', mu)
+
+  nSamples = 1000
+  w = 1.0
+  xs = np.zeros((nSamples, D))
+  prevX = np.array([5.0,10.0])
+  for nS in range(nSamples):
+    for d in range(D):
+      if d == 0:
+        def f(x): return mvn.logpdf(np.array([x, prevX[1]]), mu, Sigma)
+        prevX[0], _ = slice_univariate(f, prevX[0], w=w, nSamples=1)
+      else:
+        def f(x): return mvn.logpdf(np.array([prevX[0], x]), mu, Sigma)
+        prevX[1], _ = slice_univariate(f, prevX[1], w=w, nSamples=1)
+    xs[nS] = prevX.copy()
+  
+  xs = xs[nSamples//10:]
+  print('Sample Sigma:\n', np.cov(xs.T))
+  print('Sample mu:\n', np.mean(xs,axis=0))
+  plt.scatter(xs[:,0], xs[:,1], s=0.1, c='b')
+  plt.plot(*du.stats.Gauss2DPoints(mu, Sigma))
+  plt.axis('equal')
+  plt.show()
