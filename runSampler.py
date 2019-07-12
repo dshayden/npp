f317fc2de0b2fb19416d41caba108931e3b45f86
David Hayden <dshayden@mit.edu>
2019-07-12 15:03:17 -0400

    runSampler maxObs

--- runSampler.py
+++ runSampler.py
@@ -9,7 +9,12 @@ def main(args):
   dataset = args.dataset
 
   data = du.load(f'data/{dataset}/data')
-  y = data['y']
+  yAll = data['y']
+  if args.maxObs > 0:
+    y = [ yt[np.random.choice(range(len(yt)), min(args.maxObs, len(yt)), replace=False)] for yt in yAll ]
+  else:
+    y = yAll
+
   T = len(y)
 
   nSamples = args.nSamples
@@ -27,7 +32,7 @@ def main(args):
   
   # parametric initialization
   theta, E, S, z, pi = SED.initPartsAndAssoc(o, y, x, alpha, mL,
-    fixedBreaks=True, maxBreaks=args.nParts, nInit=5, nIter=500)
+    fixedBreaks=False, maxBreaks=args.nParts, nInit=5, nIter=500)
   Q = SED.inferQ(o, x)
   print(f'Init took {du.toc():.2f} seconds')
 
@@ -52,6 +57,13 @@ def main(args):
     if nParticles > 0:
       mL = SED.logMarginalPartLikelihoodMonteCarlo(o, y, x, theta_, E_, S_)
 
+    # sample new subset of data
+    if args.maxObs > 0:
+      y = [ yt[np.random.choice(range(len(yt)), min(args.maxObs, len(yt)), replace=False)] for yt in yAll ]
+    else:
+      y = yAll
+
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='')
   parser.add_argument('dataset', type=str, help='dataset name and path')
@@ -69,6 +81,8 @@ if __name__ == "__main__":
     help='Min number of unassociated observations to allow new part proposals')
   parser.add_argument('--mL', type=float, default=-7.0,
     help='Approximate marginal likelihood with a constant')
+  parser.add_argument('--maxObs', type=int, default=0,
+    help='Max number observations to view, 0 for all')
 
   parser.set_defaults(func=main)
 
